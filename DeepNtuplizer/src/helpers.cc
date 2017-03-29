6962a9063dee0f0ce45116cfb896411c14901523
Anna <anna.stakia@cern.ch>
2017-03-29 09:09:24 +0200

    helpers.cc <-- small editing

--- DeepNtuplizer/src/helpers.cc
+++ DeepNtuplizer/src/helpers.cc
@@ -8,43 +8,29 @@ namespace deep_ntuples {
         size_t ncs = jet.jetFlavourInfo().getcHadrons().size();
 
         if(hflav == 5) { //B jet
-            if(nbs > 1) 
-                return JetFlavor::BB;
+            if(nbs > 1) return JetFlavor::BB;
             else if(nbs == 1) {
                 for (std::vector<reco::GenParticle>::iterator it = neutrinosLepB.begin(); it != neutrinosLepB.end(); ++it){
-                    if(reco::deltaR(it->eta(),it->phi(),jet.eta(),jet.phi()) < 0.4) {
-                        return JetFlavor::LeptonicB; 
-                    }
+                    if(reco::deltaR(it->eta(),it->phi(),jet.eta(),jet.phi()) < 0.4) return JetFlavor::LeptonicB; 
                 }
                 for (std::vector<reco::GenParticle>::iterator it = neutrinosLepB_C.begin(); it != neutrinosLepB_C.end(); ++it){
-                    if(reco::deltaR(it->eta(),it->phi(),jet.eta(),jet.phi()) < 0.4) {
-                        return JetFlavor::LeptonicB_C; 
-                    }
+                    if(reco::deltaR(it->eta(),it->phi(),jet.eta(),jet.phi()) < 0.4) return JetFlavor::LeptonicB_C; 
                 }
                 return JetFlavor::B; 
             }
-            else 
-                return JetFlavor::UNDEFINED;           
+            else return JetFlavor::UNDEFINED;
         }
         else if(hflav == 4) { //C jet
-            if(ncs >= 1)
-                return JetFlavor::C;
-            else
-                return JetFlavor::UNDEFINED;
+            if(ncs >= 1) return JetFlavor::C;
+            else return JetFlavor::UNDEFINED;
         }
-        else if(hflav == 0) { //not a heavy jet
-            if(std::abs(pflav) == 5 || nbs)
-                return JetFlavor::B;
-            else if(std::abs(pflav) == 4 || ncs)
-                return JetFlavor::C;
-            else if(std::abs(pflav) == 21)
-                return JetFlavor::G;
-            else if(std::abs(pflav) == 1 || std::abs(pflav) == 2 || std::abs(pflav) == 3)
-                return JetFlavor::L;
-            else
-                return JetFlavor::UNDEFINED;
+        else if(hflav == 0) { //not a heavy hadron-flavoured jet
+            if(std::abs(pflav) == 5 || nbs) return JetFlavor::B;
+            else if(std::abs(pflav) == 4 || ncs) return JetFlavor::C;
+            else if(std::abs(pflav) == 21) return JetFlavor::G;
+            else if(std::abs(pflav) == 1 || std::abs(pflav) == 2 || std::abs(pflav) == 3) return JetFlavor::L;
+            else return JetFlavor::UNDEFINED;
             }
-        else
-            return JetFlavor::UNDEFINED; 
+        else return JetFlavor::UNDEFINED; 
     }
 }
